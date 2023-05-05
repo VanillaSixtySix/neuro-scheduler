0dba3a1b8adc582522a6249c4083fe225db58616
Vanilla Black <vanilla@f66.dev>
2023-05-05 05:25:39 -0400

    Update UX and add WIP Discord timestamp maker

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,13 @@
 	import { base } from "$app/paths";
 	import InputDay from "../components/InputDay.svelte";
     import type { InputDayEvent } from "$lib/InputDayEvent";
-    import { Button, ButtonGroup, Col, Container, FormGroup, Input, Label, Row } from "sveltestrap";
+    import { Button, ButtonGroup, Col, Container, FormGroup, Input, Label, Row, TabContent, TabPane } from "sveltestrap";
 
     let imgElemBase: HTMLImageElement;
 
     let canvas: HTMLCanvasElement;
 
-    interface DayOfWeek {
+    class DayOfWeek {
         name: string;
         title: string;
         time: string;
@@ -22,125 +22,47 @@
         imgOffline: string;
         imgElemOnline: HTMLImageElement | null;
         imgElemOffline: HTMLImageElement | null;
+
+        constructor(name: string, dateOffset: number, dateStrOffset: [[number, number], [number, number]], coords: [number, number]) {
+            this.name = name;
+            this.title = 'TBD';
+            this.time = 'TBD BST';
+            this.date = new Date(thisMonday.getFullYear(), thisMonday.getMonth(), thisMonday.getDate() + dateOffset);
+            this.dateOffset = dateStrOffset;
+            this.online = true;
+            this.coords = coords;
+            this.titleCharSpacing = 4;
+            this.imgOnline = base + '/images/online/' + name.toLowerCase() + '.png';
+            this.imgOffline = base + '/images/offline/' + name.toLowerCase() + '.png';
+            this.imgElemOnline = null;
+            this.imgElemOffline = null;
+        }
     }
 
-    const sampleTitle = 'TBD';
-    const sampleTime = 'TBD BST';
-    const sampleTitleCharSpacing = 4;
-
     const today = new Date();
-    // yyyy-mm-dd for input[type=date]
+    // input[type="date"] requires YYYY-MM-DD
     let todayStr = today.toISOString().split('T')[0];
 
     // default to this week
     const thisMonday = new Date();
     thisMonday.setDate(thisMonday.getDate() + (1 - thisMonday.getDay()));
 
+    let discordTimestamps = 'TODO';
+
     const daysOfWeek: DayOfWeek[] = [
-        {
-            name: 'Monday',
-            title: sampleTitle,
-            time: sampleTime,
-            date: new Date(thisMonday.getFullYear(), thisMonday.getMonth(), thisMonday.getDate() + (1 - thisMonday.getDay())),
-            dateOffset: [[0, 0], [0, 0]],
-            online: true,
-            coords: [1192, 78],
-            titleCharSpacing: sampleTitleCharSpacing,
-            imgOnline: base + '/images/online/monday.png',
-            imgOffline: base + '/images/offline/monday.png',
-            imgElemOnline: null,
-            imgElemOffline: null
-        },
-        {
-            name: 'Tuesday',
-            title: sampleTitle,
-            time: sampleTime,
-            date: new Date(thisMonday.getFullYear(), thisMonday.getMonth(), thisMonday.getDate() + (2 - thisMonday.getDay())),
-            dateOffset: [[0, 0], [0, 0]],
-            online: true,
-            coords: [1160, 221],
-            titleCharSpacing: sampleTitleCharSpacing,
-            imgOnline: base + '/images/online/tuesday.png',
-            imgOffline: base + '/images/offline/tuesday.png',
-            imgElemOnline: null,
-            imgElemOffline: null
-        },
-        {
-            name: 'Wednesday',
-            title: sampleTitle,
-            time: sampleTime,
-            date: new Date(thisMonday.getFullYear(), thisMonday.getMonth(), thisMonday.getDate() + (3 - thisMonday.getDay())),
-            dateOffset: [[-1, -1], [-1, -1]],
-            online: true,
-            coords: [1128, 363],
-            titleCharSpacing: sampleTitleCharSpacing,
-            imgOnline: base + '/images/online/wednesday.png',
-            imgOffline: base + '/images/offline/wednesday.png',
-            imgElemOnline: null,
-            imgElemOffline: null
-        },
-        {
-            name: 'Thursday',
-            title: sampleTitle,
-            time: sampleTime,
-            date: new Date(thisMonday.getFullYear(), thisMonday.getMonth(), thisMonday.getDate() + (4 - thisMonday.getDay())),
-            dateOffset: [[-2, 0], [0, -1]],
-            online: true,
-            coords: [1096, 505],
-            titleCharSpacing: sampleTitleCharSpacing,
-            imgOnline: base + '/images/online/thursday.png',
-            imgOffline: base + '/images/offline/thursday.png',
-            imgElemOnline: null,
-            imgElemOffline: null
-        },
-        {
-            name: 'Friday',
-            title: sampleTitle,
-            time: sampleTime,
-            date: new Date(thisMonday.getFullYear(), thisMonday.getMonth(), thisMonday.getDate() + (5 - thisMonday.getDay())),
-            dateOffset: [[-1, 0], [0, 0]],
-            online: true,
-            coords: [1065, 648],
-            titleCharSpacing: sampleTitleCharSpacing,
-            imgOnline: base + '/images/online/friday.png',
-            imgOffline: base + '/images/offline/friday.png',
-            imgElemOnline: null,
-            imgElemOffline: null
-        },
-        {
-            name: 'Saturday',
-            title: sampleTitle,
-            time: sampleTime,
-            date: new Date(thisMonday.getFullYear(), thisMonday.getMonth(), thisMonday.getDate() + (6 - thisMonday.getDay())),
-            dateOffset: [[0, 0], [0, 0]],
-            online: true,
-            coords: [1034, 789],
-            titleCharSpacing: sampleTitleCharSpacing,
-            imgOnline: base + '/images/online/saturday.png',
-            imgOffline: base + '/images/offline/saturday.png',
-            imgElemOnline: null,
-            imgElemOffline: null
-        },
-        {
-            name: 'Sunday',
-            title: sampleTitle,
-            time: sampleTime,
-            date: new Date(thisMonday.getFullYear(), thisMonday.getMonth(), thisMonday.getDate() + (7 - thisMonday.getDay())),
-            dateOffset: [[2, 0], [0, -1]],
-            online: true,
-            coords: [1004, 933],
-            titleCharSpacing: sampleTitleCharSpacing,
-            imgOnline: base + '/images/online/sunday.png',
-            imgOffline: base + '/images/offline/sunday.png',
-            imgElemOnline: null,
-            imgElemOffline: null
-        }
+        new DayOfWeek('Monday', 0, [[0, 0], [0, 0]], [1192, 78]),
+        new DayOfWeek('Tuesday', 1, [[0, 0], [0, 0]], [1160, 221]),
+        new DayOfWeek('Wednesday', 2, [[-1, -1], [-1, -1]], [1128, 363]),
+        new DayOfWeek('Thursday', 3, [[-2, 0], [0, -1]], [1096, 505]),
+        new DayOfWeek('Friday', 4, [[-1, 0], [0, 0]], [1065, 647]),
+        new DayOfWeek('Saturday', 5, [[0, 0], [0, 0]], [1034, 789]),
+        new DayOfWeek('Sunday', 6, [[2, 0], [0, -1]], [1004, 933])
     ];
 
     let ctx: CanvasRenderingContext2D;
 
     function redraw() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.clearRect(0, 0, canvas!.width, canvas!.height);
         ctx.drawImage(imgElemBase, 0, 0);
 
         for (const day of daysOfWeek) {
@@ -162,6 +84,12 @@
         canvas.height = 1080;
 
         redraw();
+
+        // Slight delay to hopefully allow fonts to load
+        setTimeout(() => {
+            updateTimestamps();
+            redraw();
+        }, 500);
     });
 
     function drawTitle(day: DayOfWeek) {
@@ -233,6 +161,7 @@
         }
         if (time != null) {
             dayOfWeek.time = time;
+            setTimeForValidStrings();
         }
         if (titleCharSpacing != null) {
             dayOfWeek.titleCharSpacing = titleCharSpacing;
@@ -273,6 +202,8 @@
         for (const day of daysOfWeek) {
             day.date = new Date(monday.getFullYear(), monday.getMonth(), monday.getDate() + daysOfWeek.indexOf(day));
         }
+        setTimeForValidStrings();
+        updateTimestamps();
         redraw();
     }
 
@@ -302,49 +233,82 @@
         link.href = canvas.toDataURL();
         link.click();
     }
+
+    function setTimeForValidStrings() {
+        const regex = /^(\d{1,2})(?::(\d{2}))?\s*([AP]M)\s*([A-Z]{2,4})?$/i;
+
+        for (const day of daysOfWeek) {
+            const match = day.time.match(regex);
+
+            console.log(day.time, match);
+
+            if (match) {
+                const hour = parseInt(match[1], 10);
+                const minute = match[2] ? parseInt(match[2], 10) : 0;
+                const isPm = match[3].toLowerCase() === 'pm';
+
+                // Check if hour and minute are valid
+                if (hour >= 1 && hour <= 12 && minute >= 0 && minute <= 59) {
+                    const newHour = isPm && hour !== 12 ? hour + 12 : (hour === 12 && !isPm) ? 0 : hour;
+                    console.log(newHour, minute);
+                    day.date.setHours(newHour, minute, 0, 0);
+                }
+            }
+        }
+    }
+
+    function updateTimestamps() {
+        // Update discordTimestamps with each day's date on a new line in the format <t:TIMESTAMP:F>
+        const timestamps = daysOfWeek.map(day => `<t:${Math.floor(day.date.getTime() / 1000)}:F>`);
+        discordTimestamps = timestamps.join('\n');
+    }
 </script>
 
 <svelte:head>
     <link rel="stylesheet" href="https://indestructibletype.com/fonts/Jost.css" type="text/css" charset="utf-8" />
 </svelte:head>
 
-<Row>
-    <Col xs="10">
-        <canvas id="canvas" bind:this={canvas}></canvas>
-    </Col>
-    <Col xs="2">
-        <div class="schedule-input">
-            <Row noGutters>
-                <Col xs="6">
-                    <Input type="date" name="start" id="start" on:change={updateDate} bind:value={todayStr} bsSize="sm" />
-                </Col>
-                <Col xs="6">
-                    <Row>
-                        <Col>
-                            <ButtonGroup class="d-flex">
-                                <Button color="secondary" size="sm" on:click={nextWeek}>Next Week</Button>
-                                <Button color="danger" size="sm" on:click={resetDate}>Reset</Button>
-                            </ButtonGroup>
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
-            <Row noGutters>
-                <Col>
-                    <ButtonGroup class="d-flex">
-                        <Button color="primary" size="sm" on:click={redraw}>Redraw</Button>
-                        <Button color="success" size="sm" on:click={copy}>Copy</Button>
-                        <Button color="success" size="sm" on:click={download}>Download</Button>
-                    </ButtonGroup>
-                </Col>
-            </Row>
-            <!-- <input type="date" id="start" name="start" value={todayStr} on:change={updateDate} /> -->
-            {#each daysOfWeek as day}
-                <InputDay day={day.name} on:message={inputDayHandler} bind:online={day.online} />
-            {/each}
-        </div>
-    </Col>
-</Row>
+<div class="outer-container">
+    <canvas id="canvas" bind:this={canvas}></canvas>
+    <div class="schedule-input">
+        <TabContent>
+            <TabPane tabId="main" tab="Main" active>
+                <Row noGutters>
+                    <Col xs="6">
+                        <Input type="date" name="start" id="start" on:change={updateDate} bind:value={todayStr} bsSize="sm" />
+                    </Col>
+                    <Col xs="6">
+                        <Row>
+                            <Col>
+                                <ButtonGroup class="d-flex">
+                                    <Button color="secondary" size="sm" on:click={nextWeek}>Next Week</Button>
+                                    <Button color="danger" size="sm" on:click={resetDate}>Reset</Button>
+                                </ButtonGroup>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+                <Row noGutters>
+                    <Col>
+                        <ButtonGroup class="d-flex">
+                            <Button color="primary" size="sm" on:click={redraw}>Redraw</Button>
+                            <Button color="success" size="sm" on:click={copy}>Copy</Button>
+                            <Button color="success" size="sm" on:click={download}>Download</Button>
+                        </ButtonGroup>
+                    </Col>
+                </Row>
+                <!-- <input type="date" id="start" name="start" value={todayStr} on:change={updateDate} /> -->
+                {#each daysOfWeek as day}
+                    <InputDay day={day.name} on:message={inputDayHandler} bind:online={day.online} />
+                {/each}
+            </TabPane>
+            <TabPane tabId="discord" tab="Discord" disabled>
+                <h3>Discord Schedule Maker</h3>
+                <Input type="textarea" name="discord" id="discord-timestamps" readonly bind:value={discordTimestamps} rows={7} />
+            </TabPane>
+        </TabContent>
+    </div>
+</div>
 
 <div class="assets">
     <img src="{base}/images/base.png" bind:this={imgElemBase} alt="NeuroSchedule Base" width="1920px" height="1080px">
@@ -357,10 +321,10 @@
 <style>
 :global(body) {
     background: #1F1F4F;
-    /* color: #DEDEDE; */
-    padding: 25px;
     font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     -webkit-font-smoothing: antialiased;
+    width: 100vw;
+    height: 100vh;
 }
 
 :global(.form-check-label, .form-label) {
@@ -387,17 +351,62 @@
     background-color: #3F3F8F;
 }
 
+:global(.tab-content .nav-link) {
+    color: #FFFFFF;
+}
+
+:global(.tab-pane) {
+    display: flex;
+    flex-direction: column;
+    padding-top: 5px;
+    gap: 5px;
+}
+
+.outer-container {
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: 100%;
+    padding: 25px;
+    gap: 25px;
+}
+
 #canvas {
     background: #2F2F5F;
     border: 2px solid #000000;
-
-    width: 100%;
+    flex-grow: 0;
+    max-width: 100vw;
+    aspect-ratio: 16 / 9;
+    height: fit-content;
+    max-height: 100%;
+    position: relative;
+    overflow: hidden;
 }
 
 .schedule-input {
     display: flex;
     flex-direction: column;
     gap: 10px;
+    min-width: 356px;
+    flex-shrink: 0;
+    overflow-x: hidden;
+    color: #FFFFFF;
+}
+
+@media (max-width: 1100px) {
+    .outer-container {
+        flex-direction: column;
+        overflow-x: hidden;
+    }
+
+    #canvas {
+        overflow: visible;
+    }
+
+    .schedule-input {
+        width: 100%;
+    }
 }
 
 .assets {
